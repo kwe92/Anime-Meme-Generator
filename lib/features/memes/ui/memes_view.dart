7f5645a97fc7c5b68cb259a5be6169d08e833d67
Kwe <Kweayon@gmail.com>
2023-08-07 17:36:27 -0500

    update: setting up initial view

--- lib/features/memes/ui/memes_view.dart
+++ lib/features/memes/ui/memes_view.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
+class MemesView extends StatelessWidget {
+  const MemesView({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('AniMemes'),
+        ),
+      ),
+    );
+  }
+}
