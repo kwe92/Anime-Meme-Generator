a52e82f59024b19208066a8bc8639c6af9c85af7
Kwe <Kweayon@gmail.com>
2023-08-08 17:59:29 -0500

    update: initial app theme

--- lib/app/themes/app_theme.dart
+++ lib/app/themes/app_theme.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+final ThemeData appTheme = ThemeData(
+  colorScheme: darkScheme,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color(0xFF43532f),
+  ),
+);
+
+const darkScheme = ColorScheme.dark();
