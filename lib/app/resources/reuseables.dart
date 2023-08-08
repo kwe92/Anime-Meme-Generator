951591b380897abae2aca325e46d5e8013c21623
Kwe <Kweayon@gmail.com>
2023-08-08 17:58:37 -0500

    Gaps

--- lib/app/resources/reuseables.dart
+++ lib/app/resources/reuseables.dart
@@ -1,1 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+// Gaps
+const gap2 = Gap(2);
+const gap4 = Gap(4);
+const gap6 = Gap(6);
+const gap8 = Gap(8);
+const gap10 = Gap(10);
+const gap12 = Gap(12);
+const gap14 = Gap(14);
+const gap16 = Gap(16);
