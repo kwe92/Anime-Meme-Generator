db53797542f98fda48a5f89c15a7a1676b0f024f
Kwe <Kweayon@gmail.com>
2023-08-08 18:28:56 -0500

    update: expanded theme with app bar outlined button and card themes | set useMaterial3 property to true | implemented parameterized resolver to shorten calls to MaterialStateProperty.resolveWith cause its way too long

--- lib/app/themes/app_theme.dart
+++ lib/app/themes/app_theme.dart
@@ -1,10 +1,28 @@
+import 'package:anime_meme_generator/app/themes/styles/colors.dart';
 import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
+  useMaterial3: true,
   colorScheme: darkScheme,
   appBarTheme: const AppBarTheme(
-    backgroundColor: Color(0xFF43532f),
+    backgroundColor: AppColors.green0,
+  ),
+  cardTheme: const CardTheme(
+    color: AppColors.green0,
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: _resolver<Color>(
+        (states) => AppColors.lightBlue0,
+      ),
+    ),
   ),
 );
 
-const darkScheme = ColorScheme.dark();
+const darkScheme = ColorScheme.dark(
+  onSurface: AppColors.lightBlue0,
+  onBackground: AppColors.lightBlue0,
+);
+
+// parameterized type to shorten resolver
+MaterialStateProperty<T?>? _resolver<T>(T Function(Set<MaterialState>) func) => MaterialStateProperty.resolveWith(func);
