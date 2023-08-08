d2ef0b49af6aba67290047a7bff90a53eca41e28
Kwe <Kweayon@gmail.com>
2023-08-08 18:01:17 -0500

    update: added appTheme to theme property

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:anime_meme_generator/app/themes/app_theme.dart';
+import 'package:anime_meme_generator/features/memes/ui/memes_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +12,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SizedBox(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        theme: appTheme,
+        home: const MemesView(),
+      );
 }
