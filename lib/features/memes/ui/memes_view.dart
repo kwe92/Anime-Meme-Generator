641fa6692719dd6c3aaf9ad6289ee53773d9c7a5
Kwe <Kweayon@gmail.com>
2023-08-08 18:27:29 -0500

    update: added outlined button

--- lib/features/memes/ui/memes_view.dart
+++ lib/features/memes/ui/memes_view.dart
@@ -22,20 +22,21 @@ class _MemesViewState extends State<MemesView> {
           child: Card(
             margin: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Padding(
-              padding: const EdgeInsets.only(top: 12),
+              padding: const EdgeInsets.symmetric(
+                vertical: 12.0,
+                horizontal: 24.0,
+              ),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   const Text('Meme Title Goes Here!'),
                   gap6,
                   SizedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Image.network('https://pm1.aminoapps.com/7598/06fd473e24ce1bd4594c6a69f37642ae4b91b04dr1-800-800v2_hq.jpg'),
-                    ),
+                    child: Image.network('https://pm1.aminoapps.com/7598/06fd473e24ce1bd4594c6a69f37642ae4b91b04dr1-800-800v2_hq.jpg'),
                   ),
                   gap12,
                   OutlinedButton(
+                    // TODO: make button work
                     onPressed: () {},
                     child: const Text('Get Next Meme'),
                   )
