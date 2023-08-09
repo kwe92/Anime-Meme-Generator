b4133833b2b95f3d1d22374ce00f3be75eeefa36
Kwe <Kweayon@gmail.com>
2023-08-09 02:58:46 -0500

    update: added build context

--- lib/features/memes/controllers/ani_meme_controller.dart
+++ lib/features/memes/controllers/ani_meme_controller.dart
@@ -1,9 +1,10 @@
 import 'package:anime_meme_generator/features/memes/models/ani_memes_model.dart';
 import 'package:anime_meme_generator/features/memes/services/memes_service.dart';
 import 'package:anime_meme_generator/features/shared/services/get_it.dart';
+import 'package:flutter/material.dart';
 
 class AniMemeModelController {
   const AniMemeModelController();
 
-  Future<AniMemesModel> getNextMeme() async => locator<AniMemesService>().getMeme();
+  Future<AniMemesModel> getNextMeme(BuildContext context) async => await locator<AniMemesService>().getMeme(context);
 }
