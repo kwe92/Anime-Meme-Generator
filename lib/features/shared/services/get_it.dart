2991a670263fbabb2d65209f6bace8cd652491ec
Kwe <Kweayon@gmail.com>
2023-08-08 23:22:51 -0500

    update: defined locator as GetIt.instance | implemented setUp function

---
+++ lib/features/shared/services/get_it.dart
@@ -0,0 +1,28 @@
+import 'package:anime_meme_generator/features/memes/controllers/ani_meme_controller.dart';
+import 'package:anime_meme_generator/features/memes/services/memes_service.dart';
+import 'package:get_it/get_it.dart';
+
+// Used to access a singleton instance of getit
+final locator = GetIt.instance;
+
+void setUp() {
+  // Lazy
+
+  //   - When something is marked "Lazy"
+  //     it is only called needed necessary and will
+  //     be removed when it is no longer being uttilized
+  //  - Lazy objects do not persist unnecessarily
+
+  locator.registerLazySingleton<AniMemesService>(
+    () => const AniMemesService(),
+  );
+
+  locator.registerLazySingleton<AniMemeModelController>(
+    () => const AniMemeModelController(),
+  );
+}
+
+// parameterized registration
+
+//   - it is better to explicitly pass the parameterized generic type
+//     to whatever register you choose to use
