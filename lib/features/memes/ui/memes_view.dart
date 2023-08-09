c0983d94c16eaeaf3978b633d0c91ba8c6f6110c
Kwe <Kweayon@gmail.com>
2023-08-09 01:32:22 -0500

    update: initial conditional rendering based on visibility of meme | used Service Locator to locate AniMemeController | gets meme and title

--- lib/features/memes/ui/memes_view.dart
+++ lib/features/memes/ui/memes_view.dart
@@ -1,5 +1,7 @@
 import 'package:anime_meme_generator/app/resources/reuseables.dart';
+import 'package:anime_meme_generator/features/memes/controllers/ani_meme_controller.dart';
 import 'package:anime_meme_generator/features/memes/models/ani_memes_model.dart';
+import 'package:anime_meme_generator/features/shared/services/get_it.dart';
 import 'package:flutter/material.dart';
 
 class MemesView extends StatefulWidget {
@@ -30,17 +32,34 @@ class _MemesViewState extends State<MemesView> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  const FittedBox(
-                    child: Text('Meme Title Goes Here!'),
-                  ),
-                  gap6,
-                  SizedBox(
-                    child: Image.network('https://pm1.aminoapps.com/7598/06fd473e24ce1bd4594c6a69f37642ae4b91b04dr1-800-800v2_hq.jpg'),
-                  ),
+                  if (visibleMeme != null)
+                    FittedBox(
+                      child: Text(visibleMeme!.title),
+                    ),
+                  if (visibleMeme != null) gap6,
+                  if (visibleMeme != null)
+                    SizedBox(
+                      child: Image.network(visibleMeme!.imgUrl),
+                    ),
+                  if (visibleMeme == null)
+                    const FittedBox(
+                      child: Text('There is not a meme loaded yet'),
+                    ),
                   gap12,
                   OutlinedButton(
                     // TODO: make button work
-                    onPressed: () {},
+                    onPressed: () async {
+                      // - from the list of registered objects pass
+                      //   the one you want to access as a parameterized type
+                      //   to your GetIt instance
+                      // - instead of instantiating the object you call its method
+                      //   through your locator
+                      final AniMemesModel aniMeme = await locator<AniMemeModelController>().getNextMeme();
+
+                      setState(() {
+                        visibleMeme = aniMeme;
+                      });
+                    },
                     child: const Text('Get Next Meme'),
                   )
                 ],
