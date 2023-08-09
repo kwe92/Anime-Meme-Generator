d811bb47edaef17ce4faf756c426b7984125cb7d
Kwe <Kweayon@gmail.com>
2023-08-09 03:01:47 -0500

    update: implemented ToastService with showSnackBar

--- lib/features/shared/utilities/toast_service.dart
+++ lib/features/shared/utilities/toast_service.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ToastService{
-
+class ToastService {
+  const ToastService();
+  void showSnackBar(BuildContext context, {required String message}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        content: Text(message),
+      ),
+    );
+  }
 }
