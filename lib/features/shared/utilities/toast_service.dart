a1cccacf55ba36a1909c14203aa00d67eb4df8b2
Kwe <Kweayon@gmail.com>
2023-08-09 19:36:48 -0500

    update: added text style to taose service

--- lib/features/shared/utilities/toast_service.dart
+++ lib/features/shared/utilities/toast_service.dart
@@ -1,3 +1,4 @@
+import 'package:anime_meme_generator/app/themes/styles/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class ToastService {
@@ -6,7 +7,11 @@ class ToastService {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: const Duration(seconds: 3),
-        content: Text(message),
+        content: Text(
+          message,
+          style: baseTextStyle,
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
