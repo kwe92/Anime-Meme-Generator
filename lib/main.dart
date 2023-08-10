2ee36c11b1316d236457f8c5fe4f6218cc7b96ab
Kwe <Kweayon@gmail.com>
2023-08-10 14:43:46 -0500

    implemented ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:anime_meme_generator/app/themes/app_theme.dart';
 import 'package:anime_meme_generator/features/memes/ui/memes_view.dart';
+import 'package:anime_meme_generator/features/memes/ui/memes_view_model.dart';
 import 'package:anime_meme_generator/features/shared/services/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   // initializing services (Initializing Service Locator in App)
@@ -12,7 +14,10 @@ void main() {
   configureDependencies();
 
   runApp(
-    const MyApp(),
+    ChangeNotifierProvider(
+      create: (context) => MemesViewModel(),
+      builder: (context, child) => const MyApp(),
+    ),
   );
 }
 
