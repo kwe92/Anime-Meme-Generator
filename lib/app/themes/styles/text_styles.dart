795906be5ec6cf5deee5e440edce0ff1610fa873
Kwe <Kweayon@gmail.com>
2023-08-09 19:35:36 -0500

    update: added basic text style theme

--- lib/app/themes/styles/text_styles.dart
+++ lib/app/themes/styles/text_styles.dart
@@ -1,3 +1,10 @@
 // ignore_for_file: unused_import
 
+import 'package:anime_meme_generator/app/themes/styles/colors.dart';
 import 'package:flutter/material.dart';
+
+const baseTextStyle = TextStyle(
+  color: AppColors.lightBlue0,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
