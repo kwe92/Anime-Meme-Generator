bc5ec408c364716922a12185076cb27a32e0852f
Kwe <Kweayon@gmail.com>
2023-08-09 19:35:16 -0500

    update: expanded app colors grey

--- lib/app/themes/styles/colors.dart
+++ lib/app/themes/styles/colors.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+/// API that holds all app colors.
 class AppColors {
   const AppColors._();
+
   static const green0 = Color(0xFF43532f);
 
   static const lightBlue0 = Color(0xFFcde4f6);
+
+  static const grey0 = Color(0x3EACACAC);
+
+  static const grey1 = Color(0xFFacacac);
 }
