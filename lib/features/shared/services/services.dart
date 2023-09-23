a66c47e04be9bd44f5c15ba0ca55e79ad8d7869b
Kwe <Kweayon@gmail.com>
2023-09-22 22:39:31 -0500

    update: edited comments and added computed getter for cient instance

--- lib/features/shared/services/services.dart
+++ lib/features/shared/services/services.dart
@@ -1,18 +1,17 @@
-import 'package:anime_meme_generator/features/memes/controllers/ani_meme_controller.dart';
 import 'package:anime_meme_generator/features/memes/services/memes_service.dart';
 import 'package:anime_meme_generator/features/shared/services/get_it.dart';
 import 'package:anime_meme_generator/features/shared/utilities/toast_service.dart';
-
-// TODO: refactor notes
-
-// - from the list of registered objects pass
-//   the one you want to access as a parameterized type
-//   to your GetIt instance
-// - instead of instantiating the object you call its method
-//   through your locator
+import 'package:http/http.dart' as http;
 
 ToastService get toastService => locator.get<ToastService>();
 
 AniMemesService get aniMemesService => locator.get<AniMemesService>();
 
-AniMemeModelController get aniMemeModelController => locator.get<AniMemeModelController>();
+http.Client get client => locator.get<http.Client>();
+
+// Retrieving Registered Singletons
+
+// - use the GetIt.get method of your instantiated GetIt instance
+//   to retrieve or create an instance of a registered type <T>
+// - instead of instantiating the object you call its method
+//   through your locator
