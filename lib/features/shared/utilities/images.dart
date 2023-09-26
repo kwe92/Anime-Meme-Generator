a210473b9d29369cca9c1043bda18588e88ecb18
Kwe <Kweayon@gmail.com>
2023-09-25 22:32:07 -0500

    implemented image class

---
+++ lib/features/shared/utilities/images.dart
@@ -0,0 +1,15 @@
+class Images {
+  const Images._();
+  static const String loading0 =
+      "/Users/kwe/flutter-projects/AnimeMemeGenerator/anime_meme_generator/assets/ab39ec-Obito-uchiha-ten-tails-jinchuriki-wallpaper-hd-1920x1440.jpg";
+
+  static const String loading1 = "/Users/kwe/flutter-projects/AnimeMemeGenerator/anime_meme_generator/assets/Paperman.full.1244182.jpg";
+
+  static const String error0 = "/Users/kwe/flutter-projects/AnimeMemeGenerator/anime_meme_generator/assets/anime_error01.jpeg";
+
+  static const String error1 = "/Users/kwe/flutter-projects/AnimeMemeGenerator/anime_meme_generator/assets/anime_error.jpeg";
+
+  static const String error2 = "/Users/kwe/flutter-projects/AnimeMemeGenerator/anime_meme_generator/assets/anime_error02.jpeg";
+
+  static const String error3 = "/Users/kwe/flutter-projects/AnimeMemeGenerator/anime_meme_generator/assets/anime_error03.jpeg";
+}
